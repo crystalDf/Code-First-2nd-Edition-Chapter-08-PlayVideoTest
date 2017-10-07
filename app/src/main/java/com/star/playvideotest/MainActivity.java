5e3506c0427bdd4133adaa1c2ee5d89b0a700b25
crystalDf <chendong333@gmail.com>
2017-10-07 22:59:28 +0800

    Chapter 08 PlayVideoTest

---
+++ app/src/main/java/com/star/playvideotest/MainActivity.java
@@ -0,0 +1,113 @@
+package com.star.playvideotest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE = 1;
+
+    private VideoView mVideoView;
+
+    private Button mPlay;
+    private Button mPause;
+    private Button mReplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mVideoView = (VideoView) findViewById(R.id.video_view);
+
+        mPlay = (Button) findViewById(R.id.play);
+        mPause = (Button) findViewById(R.id.pause);
+        mReplay = (Button) findViewById(R.id.replay);
+
+        mPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!mVideoView.isPlaying()) {
+                    mVideoView.start();
+                }
+            }
+        });
+
+        mPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mVideoView.isPlaying()) {
+                    mVideoView.pause();
+                }
+            }
+        });
+
+        mReplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mVideoView.isPlaying()) {
+                    mVideoView.resume();
+                }
+            }
+        });
+
+        if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+        } else {
+            initMediaPlayer();
+        }
+    }
+
+    private void initMediaPlayer() {
+
+        File file = new File(Environment.getExternalStorageDirectory(), "童话镇.mp4");
+
+        mVideoView.setVideoPath(file.getPath());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+
+            case REQUEST_CODE:
+                if ((grantResults.length > 0) &&
+                        (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    initMediaPlayer();
+                } else {
+                    Toast.makeText(this, "Permission Denied.", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mVideoView != null) {
+            mVideoView.suspend();
+        }
+    }
+}
